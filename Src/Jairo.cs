a6cc77d481b0d40eec239d0ca8a2c42759b9f5bf
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2024-04-18 17:39:29 -0400

    feat: add functions to jairo model

--- Src/Jairo.cs
+++ Src/Jairo.cs
@@ -55,5 +55,27 @@ namespace TEST123.Src
         public string Bitbucket { get; set; }
         public string SourceForge { get; set; }
         public string Apache { get; set; }
+
+        public Jairo()
+        {
+            Name = "Jairo";
+            LastName = "Calcina";
+            University = "Universidad Nacional de San Agustín de Arequipa";
+            Career = "Ingeniería de Sistemas";
+            Age = 20;
+            Email = "";
+        }
+
+        public Jairo(string name, string lastName, string university, string career, int age, string email)
+        {
+            Name = name;
+            LastName = lastName;
+            University = university;
+            Career = career;
+            Age = age;
+            Email = email;
+        }
+
+
     }
 }
